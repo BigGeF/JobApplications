eb66ff8ce7e16b9125e95e201308c9b3e894daf5
Hao Fan <Fan.Hao@student.greenriver.edu>
2024-04-23 00:11:41 -0400

    new file:   .htaccess         new file:   images/homeOffice.jpg         modified:   index.php         new file:   styles/styles.css         new file:   views/experience.html         new file:   views/expreience.html         modified:   views/home.html         new file:   views/info.html         new file:   views/jobLists.html

--- index.php
+++ index.php
@@ -1,27 +1,68 @@
 <?php
+session_start();
 
-// 328/hell_FatFree/index.php
-// this is my controller!
-
-//Turn on error reporting
+//add the following PHP code to turn on error reporting:
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Require
+
 require_once ('vendor/autoload.php');
 
-//Instantiate the F3 base class
 $f3 = Base::instance();
 
-
-//Define a default route
-//https://hfan.greenriverdev.com/328/JobApplications/
 $f3->route('GET /', function (){
-//    echo '<h1>Hello Fat_free!</h1>';
-    //render a view page
-    $view =new Template();
 
-    echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view ->render('views/home.html');
+});
+
+$f3->route('GET|POST /info', function ($f3) {
+
+    //check if the form has been posted
+    if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+        // Retrieve the form data from the POST request
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $email = $_POST['email'];
+        $state = $_POST['state'];
+        $phone = $_POST['phone'];
+
+        // Set the session variables using the Fat-Free Framework's set method
+        $f3->set('SESSION.firstName', $firstName);
+        $f3->set('SESSION.lastName', $lastName);
+        $f3->set('SESSION.email', $email);
+        $f3->set('SESSION.state', $state);
+        $f3->set('SESSION.phone', $phone);
+
+        // Reroute to the home page (or to another appropriate route)
+        $f3->reroute("experience");
+    }
+    $view = new Template();
+    echo $view->render('views/info.html');
+});
+
+$f3->route("GET|POST /experience", function ($f3){
+    if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+        $biography = $f3->get('POST.biography');
+        $githubLink = $f3->get('POST.githubLink');
+        $yearsExperience = $f3->get('POST.yearsExperience');
+        $relocate = $f3->get('POST.relocate');
+
+        // Store data in session using F3's set method
+        $f3->set('SESSION.biography', $biography);
+        $f3->set('SESSION.githubLink', $githubLink);
+        $f3->set('SESSION.yearsExperience', $yearsExperience);
+        $f3->set('SESSION.relocate', $relocate);
+        $f3->reroute('jobLists');
+
+    }
+    $view = new Template();
+    echo $view->render('views/experience.html');
+});
+
+$f3->route("GET|POST /jobLists", function (){
+    $view = new Template();
+    echo $view->render('views/jobList.html');
 });
 
 //run fat-free
